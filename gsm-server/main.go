ce62a1e0c7706b7dedd126c3669b9a2ba2f365de
Rafe Colton <r.colton@modcloth.com>
2013-09-14 11:11:08 -0400

    Using the logger

--- gsm-server/main.go
+++ gsm-server/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gsm"
+	gsmlog "gsm/log"
 	"os"
 )
 
+var (
+	done   = make(chan bool)
+	logger gsmlog.GsmLogger
+)
+
+func init() {
+	flag.Var(&logger, "v", "-v\t\tUse verbose output.")
+}
+
 func main() {
 	config := gsm.NewConfigurationFromFlags()
+	logger.Initialize()
 
 	if config.DisplayVersion {
 		fmt.Println(gsm.ProgVersion())
